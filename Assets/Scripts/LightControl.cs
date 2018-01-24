ba9a28b46013448af1e581116c1eebc80dac7d48
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-01-24 16:05:04 -0700

    30 Min Update
    Added:
    Light and script to turn light on and off with spacebar.

---
+++ Assets/Scripts/LightControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+    private Light myLight;
+    public bool lightState = true;
+
+
+	// Use this for initialization
+	void Start () {
+        myLight = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && lightState == true)
+        {
+            myLight.enabled = false;
+            lightState = false;
+        } else if (Input.GetKeyDown(KeyCode.Space) && lightState == false)
+        {
+            myLight.enabled = true;
+            lightState = true;
+        }
+	}
+}
